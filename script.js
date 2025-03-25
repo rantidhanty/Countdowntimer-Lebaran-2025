3cce39867c99942a9501056664277616d60016de
Rantidhanty <rantidhanty@gmail.com>
2025-03-25 23:48:34 +0700

    improvment styling

--- script.js
+++ script.js
@@ -1,127 +1,58 @@
-function _0x4a5c() {
-  const _0x16cd46 = [
-    "924700HFKdVm",
-    "https://google.com",
-    "days",
-    "6QTbFnY",
-    "getTime",
-    "querySelector",
-    "1960407hrGHVT",
-    "href",
-    ".eid-message",
-    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<div\x20class=\x22eid-message\x22>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<h2>🌙\x20Selamat\x20Hari\x20Raya\x20Idul\x20Fitri\x201446\x20H!\x20🌙</h2>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p>Semoga\x20Idul\x20Fitri\x20ini\x20membawa\x20keberkahan,\x20kedamaian,\x20dan\x20kebahagiaan\x20untuk\x20kita\x20semua.</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20<p\x20class=\x22highlight\x22>Mohon\x20maaf\x20lahir\x20dan\x20batin.\x20🤲✨</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20</div>\x0a\x20\x20\x20\x20",
-    "1603295ADNhhP",
-    "timer",
-    "March\x2031,\x202025\x2000:00:00",
-    "getElementById",
-    "6757380AERpbr",
-    "key",
-    "24vQdmQx",
-    "fade-in",
-    "hours",
-    "137438BurkuC",
-    "shiftKey",
-    "minutes",
-    "453146Udmlyd",
-    "floor",
-    "F12",
-    "3NoZZRu",
-    "addEventListener",
-    "keydown",
-    "preventDefault",
-    "location",
-    "add",
-    "ctrlKey",
-    "seconds",
-    "608605TUlFZQ",
-  ];
-  _0x4a5c = function () {
-    return _0x16cd46;
-  };
-  return _0x4a5c();
-}
-function _0xe238(_0x15a2f8, _0x5efec2) {
-  const _0x4a5cbe = _0x4a5c();
-  return (
-    (_0xe238 = function (_0xe238ce, _0x11eed0) {
-      _0xe238ce = _0xe238ce - 0xa7;
-      let _0x42b1a5 = _0x4a5cbe[_0xe238ce];
-      return _0x42b1a5;
-    }),
-    _0xe238(_0x15a2f8, _0x5efec2)
-  );
-}
-const _0x481916 = _0xe238;
-(function (_0x1a1bd3, _0x8e9a6) {
-  const _0x3539f7 = _0xe238,
-    _0x4d8908 = _0x1a1bd3();
-  while (!![]) {
-    try {
-      const _0x13e8f3 =
-        parseInt(_0x3539f7(0xaf)) / 0x1 +
-        (-parseInt(_0x3539f7(0xc6)) / 0x2) *
-          (-parseInt(_0x3539f7(0xa7)) / 0x3) +
-        -parseInt(_0x3539f7(0xb0)) / 0x4 +
-        (parseInt(_0x3539f7(0xba)) / 0x5) * (-parseInt(_0x3539f7(0xb3)) / 0x6) +
-        (-parseInt(_0x3539f7(0xc3)) / 0x7) * (parseInt(_0x3539f7(0xc0)) / 0x8) +
-        -parseInt(_0x3539f7(0xb6)) / 0x9 +
-        parseInt(_0x3539f7(0xbe)) / 0xa;
-      if (_0x13e8f3 === _0x8e9a6) break;
-      else _0x4d8908["push"](_0x4d8908["shift"]());
-    } catch (_0x57fc1f) {
-      _0x4d8908["push"](_0x4d8908["shift"]());
-    }
-  }
-})(_0x4a5c, 0xa6975);
 function updateCountdown() {
-  const _0x562dca = _0xe238,
-    _0x419c10 = new Date(_0x562dca(0xbc))[_0x562dca(0xb4)](),
-    _0x3b75b9 = new Date()["getTime"](),
-    _0x4ceba3 = _0x419c10 - _0x3b75b9;
-  if (_0x4ceba3 <= 0x0) {
-    clearInterval(countdownInterval),
-      (document[_0x562dca(0xbd)](_0x562dca(0xbb))["innerHTML"] =
-        _0x562dca(0xb9)),
-      document[_0x562dca(0xb5)](_0x562dca(0xb8))["classList"][_0x562dca(0xac)](
-        _0x562dca(0xc1)
-      );
+  const targetDate = new Date("March 31, 2025 00:00:00").getTime();
+  const now = new Date().getTime();
+  const difference = targetDate - now;
+
+  if (difference <= 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("timer").innerHTML = `
+        <div class="eid-message">
+            <h2>🌙 Selamat Hari Raya Idul Fitri 1446 H! 🌙</h2>
+            <p>Semoga Idul Fitri ini membawa keberkahan, kedamaian, dan kebahagiaan untuk kita semua.</p>
+            <p class="highlight">Mohon maaf lahir dan batin. 🤲✨</p>
+        </div>
+    `;
+    document.querySelector(".eid-message").classList.add("fade-in");
     return;
   }
-  const _0xbdf6c7 = Math[_0x562dca(0xc7)](
-      _0x4ceba3 / (0x3e8 * 0x3c * 0x3c * 0x18)
-    ),
-    _0x1dd11c = Math["floor"](
-      (_0x4ceba3 % (0x3e8 * 0x3c * 0x3c * 0x18)) / (0x3e8 * 0x3c * 0x3c)
-    ),
-    _0xe3ad64 = Math[_0x562dca(0xc7)](
-      (_0x4ceba3 % (0x3e8 * 0x3c * 0x3c)) / (0x3e8 * 0x3c)
-    ),
-    _0x2cbbe3 = Math[_0x562dca(0xc7)]((_0x4ceba3 % (0x3e8 * 0x3c)) / 0x3e8);
-  (document["getElementById"](_0x562dca(0xb2))["innerText"] = _0xbdf6c7),
-    (document[_0x562dca(0xbd)](_0x562dca(0xc2))["innerText"] = _0x1dd11c),
-    (document["getElementById"](_0x562dca(0xc5))["innerText"] = _0xe3ad64),
-    (document[_0x562dca(0xbd)](_0x562dca(0xae))["innerText"] = _0x2cbbe3);
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
 }
-const countdownInterval = setInterval(updateCountdown, 0x3e8);
-updateCountdown(),
-  document[_0x481916(0xa8)]("contextmenu", function (_0x41fc63) {
-    const _0x64a537 = _0x481916;
-    _0x41fc63[_0x64a537(0xaa)]();
-  }),
-  document["addEventListener"](_0x481916(0xa9), function (_0x36a5d4) {
-    const _0x5715f5 = _0x481916;
-    (_0x36a5d4[_0x5715f5(0xbf)] === _0x5715f5(0xc8) ||
-      (_0x36a5d4[_0x5715f5(0xad)] &&
-        _0x36a5d4[_0x5715f5(0xc4)] &&
-        _0x36a5d4["key"] === "I") ||
-      (_0x36a5d4["ctrlKey"] && _0x36a5d4[_0x5715f5(0xbf)] === "u")) &&
-      _0x36a5d4[_0x5715f5(0xaa)]();
-  }),
-  setInterval(() => {
-    const _0xf45549 = _0x481916;
-    let _0x3c8ab9 = new Date()[_0xf45549(0xb4)]();
-    debugger;
-    let _0x304fbe = new Date()[_0xf45549(0xb4)]();
-    _0x304fbe - _0x3c8ab9 > 0x64 &&
-      (window[_0xf45549(0xab)][_0xf45549(0xb7)] = _0xf45549(0xb1));
-  }, 0x3e8);
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+updateCountdown();
+
+// 🔒 Blokir Klik Kanan
+// document.addEventListener("contextmenu", function (e) {
+//   e.preventDefault();
+// });
+
+// 🔒 Blokir F12, Ctrl+Shift+I, Ctrl+U
+// document.addEventListener("keydown", function (e) {
+//   if (
+//     e.key === "F12" ||
+//     (e.ctrlKey && e.shiftKey && e.key === "I") ||
+//     (e.ctrlKey && e.key === "u")
+//   ) {
+//     e.preventDefault();
+//   }
+// });
+
+// setInterval(() => {
+//   let before = new Date().getTime();
+//   debugger;
+//   let after = new Date().getTime();
+//   if (after - before > 100) {
+//     window.location.href = "https://google.com";
+//   }
+// }, 1000);
